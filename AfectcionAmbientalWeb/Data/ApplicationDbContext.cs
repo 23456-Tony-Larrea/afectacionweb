5bdc31a63dbf39f45a53dc8c99820cf8a8ccb7fe
Anthony Vinicio <avs.larrea@yavirac.edu.ec>
2020-07-17 21:22:43 -0500

    deber02

---
+++ AfectcionAmbientalWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AfectcionAmbientalWeb.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AfectcionAmbientalWeb.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Celular> Celulars { get; set; } 
+        public DbSet<Persona> Personas { get; set; }
+    }
+}
---
+++ AfectcionAmbientalWeb/ViewModels/PersonaViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AfectcionAmbientalWeb.ViewModels
+{
+    public class PersonaViewModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = @"el ""Apellido""es obligatorio:")]
+        public string Nombre { get; set; }
+        [Required]
+        public string Apellido { get; set; }
+        [Display(Name = "Su fotografía")]
+        public IFormFile FotografiaPerfil { get; set; }
+    }
+}
